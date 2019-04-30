d33ef0042d30024ded3accb8ecc4fc5971823029
Toby Chen <chenyanjun912@hotmail.com>
2019-04-30 13:25:59 -0700

    I forgot a patch

--- App/Main.cpp
+++ App/Main.cpp
@@ -320,7 +320,13 @@ int main(int argc, char* argv[])
     SDL_Window* window;
     SDL_Init(SDL_INIT_VIDEO);
     window = SDL_CreateWindow("RHI Triangle Demo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                              640, 480, SDL_WINDOW_RESIZABLE | SDL_WINDOW_VULKAN);
+                              640, 480,
+                              SDL_WINDOW_RESIZABLE
+#if TC_OS == TC_OS_MAC_OS_X
+                                  | SDL_WINDOW_VULKAN
+#endif
+    );
+
     if (window == nullptr)
     {
         printf("Could not create window: %s\n", SDL_GetError());
